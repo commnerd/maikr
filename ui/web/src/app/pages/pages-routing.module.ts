071dd62855b5e3eb0152fe4f9cd8ec2af0ff02d3
Michael J. Miller <commnerd@gmail.com>
2022-11-07 13:38:13 -0700

    Updated packages and loaded page hierarchy correctly

--- ui/web/src/app/pages/pages-routing.module.ts
+++ ui/web/src/app/pages/pages-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PagesComponent } from './pages.component';
 
 const routes: Routes = [
   {
     path: "",
+    component: PagesComponent,
     loadChildren: () => import('@pages/list/list.module').then(m => m.ListModule)
   }
 ];
--- ui/web/src/app/pages/pages.module.ts
+++ ui/web/src/app/pages/pages.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PagesComponent } from './pages.component';
 import { PagesRoutingModule } from './pages-routing.module';
+import { CenteredContentModule } from '@templates/centered-content/centered-content.module';
+import { MainNavModule } from '@templates/main-nav/main-nav.module';
 
 
 
@@ -11,7 +13,9 @@ import { PagesRoutingModule } from './pages-routing.module';
   ],
   imports: [
     CommonModule,
-    PagesRoutingModule
+    PagesRoutingModule,
+    MainNavModule,
+    CenteredContentModule
   ]
 })
 export class PagesModule { }
