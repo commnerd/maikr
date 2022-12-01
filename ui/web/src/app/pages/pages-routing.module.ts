3f9e3b69047fdd84df0aa90d9e2b8d4e10932946
Michael J. Miller <commnerd@gmail.com>
2022-12-01 14:08:05 -0700

    Added starter pages for each route

--- ui/web/src/app/pages/pages-routing.module.ts
+++ ui/web/src/app/pages/pages-routing.module.ts
@@ -6,8 +6,38 @@ const routes: Routes = [
   {
     path: "",
     component: PagesComponent,
+    loadChildren: () => import('@pages/dashboard/dashboard.module').then(m => m.DashboardModule)
+  },
+  {
+    path: "dashboard",
+    component: PagesComponent,
+    loadChildren: () => import('@pages/dashboard/dashboard.module').then(m => m.DashboardModule)
+  },
+  {
+    path: "list",
+    component: PagesComponent,
     loadChildren: () => import('@pages/list/list.module').then(m => m.ListModule)
-  }
+  },
+  {
+    path: "lists",
+    component: PagesComponent,
+    loadChildren: () => import('@pages/lists/lists.module').then(m => m.ListsModule)
+  },
+  {
+    path: "logs",
+    component: PagesComponent,
+    loadChildren: () => import('@pages/logs/logs.module').then(m => m.LogsModule)
+  },
+  {
+    path: "schedule",
+    component: PagesComponent,
+    loadChildren: () => import('@pages/schedule/schedule.module').then(m => m.ScheduleModule)
+  },
+  {
+    path: "settings",
+    component: PagesComponent,
+    loadChildren: () => import('@pages/settings/settings.module').then(m => m.SettingsModule)
+  },
 ];
 
 @NgModule({
