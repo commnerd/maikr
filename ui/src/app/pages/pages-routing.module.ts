4c2fb485a2083c0462c4881e899c117021cafd3b
Michael J. Miller <commnerd@gmail.com>
2023-03-21 17:59:08 +1200

    Rendering drag-drop-list in pages

--- ui/src/app/pages/pages-routing.module.ts
+++ ui/src/app/pages/pages-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PagesComponent } from './pages.component';
 import { TaskListComponent } from './task-list/task-list.component';
 
 const routes: Routes = [
-    { path: '', component: TaskListComponent },
-    { path: 'tasks', component: TaskListComponent }
+    {
+      path: '',
+      component: PagesComponent,
+      children: [
+        { path: '', component: TaskListComponent },
+        { path: 'tasks', component: TaskListComponent }
+      ]
+    },
+    
 ];
 
 @NgModule({
--- ui/src/app/pages/pages.module.ts
+++ ui/src/app/pages/pages.module.ts
@@ -3,23 +3,19 @@ import { CommonModule } from '@angular/common';
 import { PagesComponent } from './pages.component';
 import { PagesRoutingModule } from './pages-routing.module';
 import { DragDropListModule } from '../components/drag-drop-list/drag-drop-list.module';
+import { TaskListComponent } from './task-list/task-list.component';
 
 
 
 @NgModule({
   declarations: [
     PagesComponent,
+    TaskListComponent,
   ],
   imports: [
     CommonModule,
     PagesRoutingModule,
-    DragDropListModule,
-  ],
-  exports: [
-    PagesComponent
-  ],
-  bootstrap: [
-    PagesComponent,
+    DragDropListModule
   ]
 })
 export class PagesModule { }
