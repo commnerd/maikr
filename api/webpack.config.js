962f0830809a83b8fa1d2e175f953dd6c5588117
Michael J. Miller <commnerd@gmail.com>
2023-10-05 04:27:13 -0500

    Started work on compiling api

--- api/webpack.config.js
+++ api/webpack.config.js
@@ -2,6 +2,8 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.ts',
+  mode: 'development',
+
   module: {
     rules: [
       {
@@ -13,6 +15,21 @@ module.exports = {
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
+    fallback: {
+        "fs": false,
+        "path": false,
+        "os": false,
+        "url": false,
+        "zlib": false,
+        "async_hooks": false,
+        "path": require.resolve("path-browserify"),
+        "util": require.resolve("util"),
+        "stream": require.resolve("stream-browserify"),
+        "buffer": require.resolve("buffer"),
+        "querystring": require.resolve("querystring-es3"),
+        "http": require.resolve("stream-http"),
+        "crypto": require.resolve("crypto-browserify")
+    }
   },
   output: {
     filename: 'bundle.js',
